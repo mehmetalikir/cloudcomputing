df8c477f5df5be97e3f970e267e132f5d7d21880
Mehmet Ali KIR <37167888+mehmetalikir@users.noreply.github.com>
2022-07-21 22:45:44 +1000

    implementing Azure interaction

--- 03_python-for-sysadmin/pgbackup/src/pgbackup/storage.py
+++
@@ -1,7 +0,0 @@
-def local(infile, outfile):
-    outfile.write(infile.read())
-    outfile.close()
-    infile.close()
-
-def azure(client, infile, bucket, filename):
-    client.upload_fileobj(infile, bucket, filename)
