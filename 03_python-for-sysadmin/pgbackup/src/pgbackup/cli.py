052973a5a7300a8b6eeed49fc65705ae64486240
Mehmet Ali KIR <37167888+mehmetalikir@users.noreply.github.com>
2022-07-21 21:12:35 +1000

    implementing CLI guided by tests

---
+++ 03_python-for-sysadmin/pgbackup/src/pgbackup/cli.py
@@ -0,0 +1,40 @@
+from argparse import Action, ArgumentParser
+
+known_drivers = ['local', 'azure']
+
+class DriverAction(Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        driver, destination = values
+        if driver.lower() not in known_drivers:
+            parser.error("Unknown driver. Available drivers are 'local' & 'azure'")
+        namespace.driver = driver.lower()
+        namespace.destination = destination
+
+def create_parser():
+    parser = ArgumentParser()
+    parser.add_argument('url', help="URL of the PostgreSQL database to backup")
+    parser.add_argument('--driver', '-d',
+            help="how & where to store the backup",
+            nargs=2,
+            action=DriverAction,
+            metavar=('driver', 'destination'),
+            required=True)
+    return parser
+
+def main():
+    import time
+    import boto3
+    from pgbackup import pgdump, storage
+
+    args = create_parser().parse_args()
+    dump = pgdump.dump(args.url)
+    if args.driver == 'azure':
+        client = boto3.client('azure')
+        timestamp = time.strftime("%Y-%m-%dT%H:%M:%S", time.localtime())
+        file_name = pgdump.dump_file_name(args.url, timestamp)
+        print(f"Backing database up to {args.destination} in AZURE as {file_name}")
+        storage.s3(client, dump.stdout, args.destination, file_name)
+    else:
+        outfile = open(args.destination, 'wb')
+        print(f"Backing database up locally to {args.destination}")
+        storage.local(dump.stdout, outfile)
